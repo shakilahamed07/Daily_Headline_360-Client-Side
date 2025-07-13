c68e981a0aa77e1c925e02cab134346b8b960c7c
shakilahamed07 <shakil200607@gmail.com>
2025-07-14 00:55:33 +0600

    update navbar, Header logo and advertise

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Share/Navbar';
 import Footer from '../Components/Share/Footer';
+import Header from '../Pages/Home/Header/Header';
 
 
 const MainLayouts = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            <header>
+                <Header/>
+                <Navbar/>
+            </header>
             <main className='min-h-[calc(100vh-(285px))] max-w-[1350px] mx-auto '>
                 <Outlet></Outlet>
             </main>
