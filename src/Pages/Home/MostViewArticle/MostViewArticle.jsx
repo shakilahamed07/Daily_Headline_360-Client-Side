041362a0ba6a21dd5a985ed8844098d965f73f13
shakilahamed07 <shakil200607@gmail.com>
2025-08-17 12:18:03 +0600

    update carousel

--- src/Pages/Home/MostViewArticle/MostViewArticle.jsx
+++ src/Pages/Home/MostViewArticle/MostViewArticle.jsx
@@ -4,9 +4,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import Loader from "../../../Components/Share/Loader";
+import { useNavigate } from "react-router";
 
 const MostViewArticle = () => {
   const axiosSecure = useAxiosSecure();
+  const navigate = useNavigate()
 
   const { data: trendingArticles = [], isLoading} = useQuery({
     queryKey: ["trendingArticles"],
@@ -16,31 +18,37 @@ const MostViewArticle = () => {
     },
   });
 
+  const handelViwDetails  = (id) =>{
+      navigate(`/article/${id}`)
+  }
+
 
   if (isLoading) return <Loader/>;
 
   return (
-    <div className="mx-2 mt-5 shadow-sm rounded-xl mb-10">
+    <div className="mx-2 mt-5 shadow-sm rounded-2xl mb-10">
       <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} interval={4000}
         stopOnHover={false}
         >
         
         {trendingArticles.map((article) => (
-          <div key={article._id} className="rounded-xl overflow-hidden">
+          <div key={article._id} className="flex" onClick={()=>handelViwDetails(article._id)}>
+            <div className="rounded-2xl overflow-hidden bg-base-200">
             <img
               src={article.image}
               alt={article.title}
               className="md:h-96 h-52 w-full object-center"
             />
-            <div className="p-4 text-left mb-5">
-              <h3 className="text-3xl font-semibold mb-1">{article.title}</h3>
-              <p className="text-sm text-gray-500 mb-5">
+            <div className="p-4 text-left mb-5 bg-base-200">
+              <h3 className="md:text-4xl text-3xl font-semibold mb-1 text-center line-clamp-1">{article.title}</h3>
+              <p className="mb-5 text-center mt-2">
                 Publisher: {article.publisher}
               </p>
-              <p className="text-sm text-gray-600 line-clamp-3 mb-5">
+              <p className="text-sm line-clamp-3 mb-5 text-center">
                 {article.description}
               </p>
             </div>
+          </div>
           </div>
         ))}
       </Carousel>
